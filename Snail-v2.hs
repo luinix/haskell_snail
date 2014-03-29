a0c9e0241742240ddb2cb4bdcb67757e93c815b8
Luis Rivas Vañó <luinux@gmail.com>
2014-03-29 15:13:28 -0500

    Another approach: first the perimeter, recursive call with the inner matrix

---
+++ Snail-v2.hs
@@ -0,0 +1,30 @@
+module Snail (snail) where
+
+type Matrix = [[Integer]]
+
+inner :: [a] -> [a]
+inner = tail . init
+
+goRight :: Matrix -> [Integer]
+goRight = head
+
+goDown :: Matrix -> [Integer]
+goDown = tail . map last
+
+goLeft :: Matrix -> [Integer]
+goLeft = tail . reverse . last
+
+goUp :: Matrix -> [Integer]
+goUp = reverse . init . tail . map head
+
+perimeter :: Matrix -> [Integer]
+perimeter x = head x ++ goDown x ++ goLeft x ++ goUp x
+
+submatrix :: Matrix -> Matrix
+submatrix = inner . map inner
+
+snail :: Matrix -> [Integer]
+snail [] = []
+snail (x:[]) = x
+snail ((x:[]):xs) = [x] ++ (map head xs)
+snail x = perimeter x ++ snail ( submatrix x )
